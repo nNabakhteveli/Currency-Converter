011d471cfadd08f0513042aa9bf682d3c8390a5b
Nabakhteveli <nabakhtevelin@gmail.com>
2020-12-22 01:41:16 +0400

    Adding more style using SCSS

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let dollarCurreny = 3.2811;
+let dollarCurrency = 3.2811;
 let euroCurrency = 4.0043;
 let rubCurrency = 0.0447;
 let tryCurrency = 0.421;
 
+
 function process() {
     let input = Number(document.form.InputName.value);
     // Dollar($) 
-    document.getElementById('USDoutput').innerHTML = "$" + input / dollarCurreny;
+    document.getElementById('USDoutput').innerHTML = "$" + input / dollarCurrency;
     // Euro(€)
     document.getElementById('EUROoutput').innerHTML = "€" + input / euroCurrency;
     // RUB(₽)
