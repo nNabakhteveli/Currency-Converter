cf2b25a5c38cca250cad0165e0f7f67efa863c1a
Nabakhteveli <nabakhtevelin@gmail.com>
2020-12-23 23:07:20 +0400

    Fixed alert bug and added "Clear" button

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ let euroCurrency = 4.0043;
 let rubCurrency = 0.0447;
 let tryCurrency = 0.421;
 
-
-function process() {
+function process() {    
     let input = Number(document.form.InputName.value);
-    // Dollar($) 
     let usd = document.getElementById('USDoutput');
     usd.innerHTML = "$" + (input / dollarCurrency).toFixed(2);
-
     // Euro(€)
     let euro = document.getElementById('EUROoutput');
     euro.innerHTML = "€" + (input / euroCurrency).toFixed(2);
@@ -19,13 +16,4 @@ function process() {
     // TRY(₺)
     let ttry = document.getElementById('TRYoutput');
     ttry.innerHTML = "₺" + (input / tryCurrency).toFixed(2);
-
-    if(typeof input != Number){
-        alert("შეიყვანეთ თანხის ვალიდური რაოდენობა");
-        usd.innerHTML = ""; 
-        euro.innerHTML = ""; 
-        rub.innerHTML = ""; 
-        ttry.innerHTML = ""; 
-        window.location.reload();
-    }
 }
